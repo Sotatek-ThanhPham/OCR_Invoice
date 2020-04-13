ffa342af5ae8fc02fd914e8e8b9299eaf1f98558
sotatek-thanhpham <thanh.pham@sotatek.com>
2020-04-13 22:36:45 +0700

    update code

--- OCR_Invocie/demoTemplate/views.py
+++ OCR_Invocie/demoTemplate/views.py
@@ -93,43 +93,14 @@ def index(request):
     res = read_image(filename).split("\n")
     return JsonResponse(res, safe=False)
 
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
+#image to text GG Cloud Vision
 
-
-def convert_pdf_to_txt(filename):
-    path = '/home/totoro0098/PycharmProjects/OCR/OCR_Invocie/demoTemplate/templates/' + filename
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
-
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
-                                  check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-    print("==============")
-    print(text)
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
+import os
+from google.cloud import vision
+from google.cloud import storage
+from google.protobuf import json_format
 
 def read_image(path, bucket_name="cloud-vision-84893", language="en"):
-    import os
-    from google.cloud import vision
-    from google.cloud import storage
-    from google.protobuf import json_format
 
     # Supported mime_types are: 'application/pdf' and 'image/tiff'
     mime_type = "application/pdf"
@@ -193,6 +164,40 @@ def read_image(path, bucket_name="cloud-vision-84893", language="en"):
     return annotation.text.encode("utf-8")
 
 
+#PDF to text
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+
+def convert_pdf_to_txt(filename):
+    path = '/home/totoro0098/PycharmProjects/OCR/OCR_Invocie/demoTemplate/templates/' + filename
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+    print("==============")
+    print(text)
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
 from pdfminer.layout import LAParams, LTTextBox
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager
@@ -235,71 +240,71 @@ def getFirstData(t):
 def takeSecond(elem):
     return elem[1]
 
-
-def getTemp(data):
-    arrTemp = []
-    for num, i in enumerate(data, start=0):
-        a = i.lower().strip()
-        if a in TEMPLATE_INFORMATION:
-            arrTemp.append(("information", num))
-        elif a in TEMPLATE_SUMMARY:
-            arrTemp.append(("summary", num))
-        elif a in TEMPLATE_SKILLS:
-            arrTemp.append(("skills", num))
-        elif a in TEMPLATE_EXPERIENCE:
-            arrTemp.append(("experience", num))
-        elif a in TEMPLATE_EDUCATION:
-            arrTemp.append(("education", num))
-        elif a in TEMPLATE_AWARDS:
-            arrTemp.append(("awards", num))
-        elif a in TEMPLATE_REFERENCES:
-            arrTemp.append(("references", num))
-        elif a in TEMPLATE_INTERESTS:
-            arrTemp.append(("interests", num))
-    if (arrTemp[0][0] != "information" and arrTemp[0][1] != 0):
-        arrTemp.append(("information", 0))
-    arrTemp.sort(key=takeSecond)
-    return arrTemp
-
-
-def getInfor(data, start, end):
-    paragraphData = "".join(data[start:end])
-    print(paragraphData)
-    list_name = re.findall(
-        r"((([ ]*[A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯĂẠẢẤẦẨẪẬÂẮẰẲẴẶẸẺẼỀỀỂỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪỬỮỰỲỴÝỶỸ]{2,})+)+|([ ]*[A-Z][a-z_àáâãèéêìíòóôõùúăđĩũơưăạảấầẩẫậắằẳâấầẫậẵặẹẻẽềềểễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+)+)",
-        paragraphData)
-    list_phone = re.findall(r'(\d{10})', paragraphData)
-    list_email = re.findall(r'(\w+@(gmail.com|yahoo.com|vnu.edu.vn))', paragraphData)
-    # print("------------------")
-    # print(list_name)
-    # print(list_phone)
-    # print(list_email)
-    if list(map(getFirstData, list_name)):
-        name = list(map(getFirstData, list_name))[0]
-    else:
-        name = "No Name"
-    if list(list_phone):
-        phone = list(list_phone)[0]
-    else:
-        phone = "No Phone"
-    if list(map(getFirstData, list_email)):
-        email = list(map(getFirstData, list_email))[0]
-    else:
-        email = "No Email"
-    data = {
-        "Name": name,
-        "Phone": phone,
-        "Email": email
-    }
-    return data
-
-
-def getSkills(data, start, end):
-    paragraphData = " ".join(data[start:end])
-    skills_1 = []
-    skills_2 = []
-    for i in LIST_SKILLS_1:
-        if i in paragraphData: skills_1.append(i)
-    # for i in LIST_SKILLS_2:
-    #   if i in paragraphData: skills_2.append(i)
-    return {"skills_1": skills_1}
+#
+# def getTemp(data):
+#     arrTemp = []
+#     for num, i in enumerate(data, start=0):
+#         a = i.lower().strip()
+#         if a in TEMPLATE_INFORMATION:
+#             arrTemp.append(("information", num))
+#         elif a in TEMPLATE_SUMMARY:
+#             arrTemp.append(("summary", num))
+#         elif a in TEMPLATE_SKILLS:
+#             arrTemp.append(("skills", num))
+#         elif a in TEMPLATE_EXPERIENCE:
+#             arrTemp.append(("experience", num))
+#         elif a in TEMPLATE_EDUCATION:
+#             arrTemp.append(("education", num))
+#         elif a in TEMPLATE_AWARDS:
+#             arrTemp.append(("awards", num))
+#         elif a in TEMPLATE_REFERENCES:
+#             arrTemp.append(("references", num))
+#         elif a in TEMPLATE_INTERESTS:
+#             arrTemp.append(("interests", num))
+#     if (arrTemp[0][0] != "information" and arrTemp[0][1] != 0):
+#         arrTemp.append(("information", 0))
+#     arrTemp.sort(key=takeSecond)
+#     return arrTemp
+#
+#
+# def getInfor(data, start, end):
+#     paragraphData = "".join(data[start:end])
+#     print(paragraphData)
+#     list_name = re.findall(
+#         r"((([ ]*[A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯĂẠẢẤẦẨẪẬÂẮẰẲẴẶẸẺẼỀỀỂỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪỬỮỰỲỴÝỶỸ]{2,})+)+|([ ]*[A-Z][a-z_àáâãèéêìíòóôõùúăđĩũơưăạảấầẩẫậắằẳâấầẫậẵặẹẻẽềềểễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+)+)",
+#         paragraphData)
+#     list_phone = re.findall(r'(\d{10})', paragraphData)
+#     list_email = re.findall(r'(\w+@(gmail.com|yahoo.com|vnu.edu.vn))', paragraphData)
+#     # print("------------------")
+#     # print(list_name)
+#     # print(list_phone)
+#     # print(list_email)
+#     if list(map(getFirstData, list_name)):
+#         name = list(map(getFirstData, list_name))[0]
+#     else:
+#         name = "No Name"
+#     if list(list_phone):
+#         phone = list(list_phone)[0]
+#     else:
+#         phone = "No Phone"
+#     if list(map(getFirstData, list_email)):
+#         email = list(map(getFirstData, list_email))[0]
+#     else:
+#         email = "No Email"
+#     data = {
+#         "Name": name,
+#         "Phone": phone,
+#         "Email": email
+#     }
+#     return data
+#
+#
+# def getSkills(data, start, end):
+#     paragraphData = " ".join(data[start:end])
+#     skills_1 = []
+#     skills_2 = []
+#     for i in LIST_SKILLS_1:
+#         if i in paragraphData: skills_1.append(i)
+#     # for i in LIST_SKILLS_2:
+#     #   if i in paragraphData: skills_2.append(i)
+#     return {"skills_1": skills_1}
